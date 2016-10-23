b2b6c1721081367a48eb233b8c2f6783119787e9
Mark Wigzell <markwigzell@gmail.com>
2016-10-23 16:16:49 -0700

    added Mockito, Dagger, Junit, Espresso

---
+++ mobile/src/main/java/com/test/mwigzell/test/Calculator.java
@@ -0,0 +1,23 @@
+package com.test.mwigzell.test;
+
+/**
+ * Created by mwigzell on 10/23/16.
+ */
+public class Calculator {
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double subtract(double a, double b){
+        return a - b;
+    }
+
+    public double divide(double a, double b){
+        return a / b;
+    }
+
+    public double multiply(double a, double b){
+        return a * b;
+    }
+}
